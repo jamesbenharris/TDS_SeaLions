f8f9eb40a6b591248e2aca247e3151efea15a818
Ben Harris <james.ben.harris@gmail.com>
2017-04-05 02:58:22 -0500

    Added Extract Sea Lions

--- preprocessing.py
+++ preprocessing.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import time
+import pandas as pd
 from skimage.color import rgb2gray,rgba2rgb
 from skimage import data,io
 from skimage import feature
@@ -23,10 +24,9 @@ def processImg(img):
     orig2 = replaceColor(orig.copy())
     print("--- Cleaned image in %s seconds ---" % (time.time() - start_time))
     gray = rgb2gray(orig2)
-    
     #Find Edges
     start_time = time.time()
-    edges3 = feature.canny(gray, sigma=3,low_threshold=.1,high_threshold=.15)
+    edges3 = feature.canny(gray, sigma=4,low_threshold=.1,high_threshold=.15)
     print("--- Found edges in %s seconds ---" % (time.time() - start_time))
     #edges3 = roberts(image)
     #io.imshow(edges3)
@@ -34,24 +34,33 @@ def processImg(img):
     
     #Find Contours
     start_time = time.time()
-    contours = find_contours(edges3, .8, fully_connected='high', positive_orientation='low')
+    contours = find_contours(edges3, .5, fully_connected='high', positive_orientation='low')
     print("--- Found contours in %s seconds ---" % (time.time() - start_time))
     return contours,orig,orig2,edges3
 
-def extractSeaLions(contours):
+def extractSeaLions(contours,orig,orig2):
     start_time = time.time()
+    df = []
+    i = 0
+    f = 0
     #Filter contours by color and shape. Plot Image with squares
     for n, contour in enumerate(contours):
         x_c,y_c = getCenter(contour)
-        x,y,w,h = getSquare(contour)
         boolean = getBool(orig2[y_c,x_c])
+        truth,cl,scale = getColor(orig[y_c,x_c])
+        x,y,w,h = getSquare(contour,scale)
+        i = (y,y+h,x,x+w)
         #ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
-        if(boolean and abs(w-h)<10):
-            color = getColor(orig[y_c,x_c])[1]
-                        
+        if(boolean and abs(w-h)<10 and i != f):
+            crop = orig[y:y+h,x:x+w]
+            row = [crop,cl]
+            df.append(row)
+            f = (y,y+h,x,x+w)
     print("--- Extracted Sea Lions in %s seconds ---" % (time.time() - start_time))
+    return df
 
-def findSeaLions(contours,orig,orig2,shape):
+
+def findSeaLions(contours,orig,orig2):
     start_time = time.time()
     fig, ax = plt.subplots()
     ax.imshow(orig)
@@ -63,8 +72,10 @@ def findSeaLions(contours,orig,orig2,shape):
         x,y,w,h = getSquare(contour,scale)
         #ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
         if(boolean and abs(w-h)<5):
-            circ = plt.Circle((x_c, y_c), radius=.1, color='b')
+            circ = plt.Circle((x, y), radius=3, color='b')
+            circ2 = plt.Circle((x+w, y+h), radius=3, color='r')
             ax.add_patch(circ)
+            ax.add_patch(circ2)
             ax.add_patch(
             patches.Rectangle(
                 (x, y),
--- processImages.py
+++ processImages.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr  4 00:47:51 2017
+Created on Tue Apr  4 00:47:51 2017d
 
 @author: benharris
 """
@@ -10,5 +10,16 @@ from preprocessing import *
 
 results,image,image_cleaned,edges  = processImg('/Users/benharris/Documents/Projects/SeaLions/images/0.jpg')
 
-image_complete = findSeaLions(results,image,image_cleaned,image.shape)
+findSeaLions(results,image,image_cleaned)
+
+images = extractSeaLions(results,image,image_cleaned)
+
+io.imshow(edges)
+io.show()
+        
+for img in images[:]:
+    if len(img[0])>0 and img[1]==4:
+        io.imshow(img[0])
+        io.show()
+        break
 
